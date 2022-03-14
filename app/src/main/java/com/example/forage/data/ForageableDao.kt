1bd54cbe2f8966fc5b445b0f47f1df64284a7ed3
Christopher Nicholson <nicholsonc@gmail.com>
2022-03-14 02:39:21 -0400

    updated room per https://stackoverflow.com/questions/56974137/room-error-not-sure-how-to-handle-insert-methods-return-type and it works

--- app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -15,7 +15,14 @@
  */
 package com.example.forage.data
 
+import com.example.forage.model.Forageable
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access Object for database interaction.
@@ -24,13 +31,23 @@ import androidx.room.Dao
 interface ForageableDao {
 
     // TODO: implement a method to retrieve all Forageables from the database
+    @Query("SELECT * FROM forageable_database")
+    fun getForageables(): Flow<List<Forageable>>
 
     // TODO: implement a method to retrieve a Forageable from the database by id
+    @Query("SELECT * FROM forageable_database WHERE id = :id")
+    fun getForageable(id: Long): Flow<Forageable>
 
     // TODO: implement a method to insert a Forageable into the database
     //  (use OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(forageable: Forageable)
 
     // TODO: implement a method to update a Forageable that is already in the database
+    @Update
+    suspend fun update(forageable: Forageable)
 
     // TODO: implement a method to delete a Forageable from the database.
+    @Delete
+    suspend fun delete(forageable: Forageable)
 }
