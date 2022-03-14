c8ec455102446ef3d09210b8740a8fc66869aff2
Christopher Nicholson <nicholsonc@gmail.com>
2022-03-14 17:36:58 -0400

    ForageableViewModel refactored, including typo fix

--- app/src/main/java/com/example/forage/ui/ForageableListFragment.kt
+++ app/src/main/java/com/example/forage/ui/ForageableListFragment.kt
@@ -22,10 +22,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.example.forage.BaseApplication
 import com.example.forage.R
 import com.example.forage.databinding.FragmentForageableListBinding
 import com.example.forage.ui.adapter.ForageableListAdapter
 import com.example.forage.ui.viewmodel.ForageableViewModel
+import com.example.forage.ui.viewmodel.ForageableViewModelFactory
 
 /**
  * A fragment to view the list of [Forageable]s stored in the database.
@@ -37,7 +39,11 @@ class ForageableListFragment : Fragment() {
     // TODO: Refactor the creation of the view model to take an instance of
     //  ForageableViewModelFactory. The factory should take an instance of the Database retrieved
     //  from BaseApplication
-    private val viewModel: ForageableViewModel by activityViewModels()
+    private val viewModel: ForageableViewModel by activityViewModels {
+        ForageableViewModelFactory(
+            (activity?.application as BaseApplication).database.forageableDao()
+        )
+    }
 
     private var _binding: FragmentForageableListBinding? = null
 
